afd62b35113673f0e1eb3a124dbab7f203fec954
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2015-02-08 15:03:08 +0100

    Output csv

--- src/App.kt
+++ src/App.kt
@@ -11,17 +11,13 @@ class Column(val title: String, val content: (Int) -> Double) {
 
 class Sheet(var content: List<Column>) {
     fun print() {
-
         for (c in content) {
-            print("\t\t${c.title}")
+            print("${c.title};")
         }
-
         println()
-
-        for (i in 0..10) {
+        for (i in 0..1000) {
             for (c in content) {
-
-                print("\t\t${c.eval(i)}")
+                print("${c.eval(i)};")
             }
             println()
         }
@@ -30,16 +26,13 @@ class Sheet(var content: List<Column>) {
 
 fun powerOfTwo(x: Int): Double {
     if (x == 0)
-        return 1.0;
+        return 1.0
     else
         return powerOfTwo(x - 1) * 2.0
 }
 
-
 fun main(args: Array<String>) {
-
     var numberOfCpus: Column = Column("Number of CPUs", ::powerOfTwo)
-
     var nX: Column = Column("Problem Size X-Dimension", { 100.0 })
     var nY: Column = Column("Problem Size Y-Dimension", { 100.0 })
     var tA: Column = Column("Calculation Time per Cell", { 10.0 })
@@ -52,25 +45,21 @@ fun main(args: Array<String>) {
                 tA.eval(x) * numberOfOperations.eval(x) + // Time to calculate each cell
                 tK.eval(x) * numberOfCpus.eval(x) // Communication increases with number of CPUs
     }
-
-
     var tP: Column = Column("Parallel Time", ::timeParallel)
 
     fun timeSequential(x: Int): Double {
         return nX.eval(x) * nY.eval(x) * tA.eval(x) * numberOfOperations.eval(x)
     }
-
     var tS: Column = Column("Sequential Time", ::timeSequential)
 
     fun calculateSpeedup(x: Int): Double {
         return timeSequential(x) / timeParallel(x)
     }
+    var speedup: Column = Column("Speedup", ::calculateSpeedup)
 
     fun calculateEfficiency(x: Int): Double {
         return calculateSpeedup(x) / numberOfCpus.eval(x)
     }
-
-    var speedup: Column = Column("Speedup", ::calculateSpeedup)
     var efficiency: Column = Column("Efficiency", ::calculateEfficiency)
 
     var sheet = Sheet(linkedListOf(numberOfCpus, nX, nY, tA, numberOfOperations, tK, tP, tS, speedup, efficiency))
