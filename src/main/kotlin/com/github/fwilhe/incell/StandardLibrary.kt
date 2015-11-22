72dbd58287570d5607262e7d0ef6f571905adeaf
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2015-11-22 11:07:23 +0100

    Remove currenty unused code

--- src/main/kotlin/com/github/fwilhe/incell/StandardLibrary.kt
+++ src/main/kotlin/com/github/fwilhe/incell/StandardLibrary.kt
@@ -10,9 +10,3 @@ fun logarithm(x: Int): Double = Math.log(x.toDouble())
 fun absolute(x: Int): Double = Math.abs(x.toDouble())
 fun random(x: Int): Double = Math.random()
 fun powerOfTwo(x: Int): Double = Math.pow(x.toDouble(), 2.0)
-
-fun extractFrom(function: (Int) -> Double, lowerIndex: Int, upperIndex: Int, results: List<Double> = arrayListOf()): DoubleArray {
-    if (lowerIndex > upperIndex) return results.toDoubleArray()
-
-    return com.github.fwilhe.incell.extractFrom(function, lowerIndex + 1, upperIndex, results + function(lowerIndex))
-}
--- src/main/kotlin/com/github/fwilhe/incell/example/Simple.kt
+++ src/main/kotlin/com/github/fwilhe/incell/example/Simple.kt
@@ -10,10 +10,6 @@ fun arbitrary(x: Int): Double {
     }
 }
 
-fun sum(vararg arguments: Double): Double {
-    return arguments.sum().toDouble()
-}
-
 fun main(args: Array<String>) {
     val countUp: Column = Column("Count", ::count)
     val constantValue: Column = Column("Constant Value", { 42.23 })
@@ -25,8 +21,6 @@ fun main(args: Array<String>) {
     val logarithm: Column = Column("log()", ::logarithm)
     val absolute: Column = Column("abs()", ::absolute)
     val random: Column = Column("Random", ::random)
-
-    val someValues: DoubleArray = extractFrom(::count, 3, 6)
 
     runSheet(arrayOf(countUp, constantValue, arbitraryValue, evenValue, sine, cosine, tangent, logarithm, absolute, random))
 }
