0c2c2c751ea383a37a4a9e5a0f8c087b0859eab6
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2017-11-18 18:45:11 +0100

    Add Junit 5 to the project

---
+++ src/test/kotlin/com/github/fwilhe/incell/Tests.kt
@@ -0,0 +1,12 @@
+package com.github.fwilhe.incell
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class Tests {
+
+    @Test
+    fun t() {
+        assertTrue(true)
+    }
+}
