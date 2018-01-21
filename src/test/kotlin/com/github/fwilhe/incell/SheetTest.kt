6f63f86d3fe8ff267a9a59e7a786d17ac50301bf
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2018-01-21 21:25:55 +0100

    Add test for fromArray

--- src/test/kotlin/com/github/fwilhe/incell/SheetTest.kt
+++ src/test/kotlin/com/github/fwilhe/incell/SheetTest.kt
@@ -2,6 +2,8 @@ package com.github.fwilhe.incell
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class SheetTest {
     @Test
@@ -9,5 +11,22 @@ internal class SheetTest {
         val expected = Sheet(listOf(Column("constant value", { 1.11 }))).row(0)
         val sheet = spreadsheet { column("constant value", { 1.11 }) }.row(0)
         assertEquals(expected, sheet)
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2])
+    fun fromArrayTest(i: Int) {
+
+        fun expected(x: Int): Double = when (x) {
+            0 -> 2.3
+            1 -> 1.3
+            2 -> 7.2
+            else -> throw RuntimeException("Test failed, actual array has only 3 entries.")
+        }
+
+        val actual: columnFunction = fromArray(arrayOf(2.3, 1.3, 7.2))
+
+        assertEquals(expected(i), actual(i))
     }
 }
