716a0c9bc2a5a5ac094924caea33b5e7ceedb885
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2018-03-10 16:35:48 +0100

    Use Kotlin math functions for use on JVM and JS

--- src/main/kotlin/com/github/fwilhe/incell/StandardLibrary.kt
+++ src/main/kotlin/com/github/fwilhe/incell/StandardLibrary.kt
@@ -1,12 +1,13 @@
 package com.github.fwilhe.incell
 
+import kotlin.math.pow
+
 fun isEven(x: Int): Double = if (x.rem(2) == 0) 1.0 else 0.0
 fun count(x: Int): Double = x.toDouble()
 
-fun sine(x: Int): Double = Math.sin(x.toDouble())
-fun cosine(x: Int): Double = Math.cos(x.toDouble())
-fun tangent(x: Int): Double = Math.tan(x.toDouble())
-fun logarithm(x: Int): Double = Math.log(x.toDouble())
-fun absolute(x: Int): Double = Math.abs(x.toDouble())
-fun random(x: Int): Double = Math.random()
-fun powerOfTwo(x: Int): Double = Math.pow(x.toDouble(), 2.0)
+fun sine(x: Int): Double = kotlin.math.sin(x.toDouble())
+fun cosine(x: Int): Double = kotlin.math.cos(x.toDouble())
+fun tangent(x: Int): Double = kotlin.math.tan(x.toDouble())
+fun logarithm(x: Int): Double = kotlin.math.ln(x.toDouble())
+fun absolute(x: Int): Double = kotlin.math.abs(x.toDouble())
+fun powerOfTwo(x: Int): Double = x.toDouble().pow(2)
