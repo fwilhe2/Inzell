69a3adea01873dae5f36b8dba951f6d24b3881de
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-08-17 23:18:19 +0200

    wip render html table

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,14 +1,49 @@
-import com.github.fwilhe.inzell.*
+import com.github.fwilhe.inzell.Column
+import com.github.fwilhe.inzell.powerOfTwo
+import kotlinx.browser.*
+import kotlinx.html.*
+import kotlinx.html.dom.*
 
 fun main() {
-    val expenseList = listOf(55.3, 82.1, 2.8, 9.0)
-    val expenses = buildFunctionOf(expenseList)
-    fun shareOfExpense(x: Int): Double = expenses(x) / expenseList.sum() * 100
-    fun expenseWithTaxes(x: Int): Double = expenses(x) * 1.15
+    val numberOfCpus = Column("Number of CPUs", ::powerOfTwo)
+    val nX = Column("Problem Size X-Dimension") { 100.0 }
+    val nY = Column("Problem Size Y-Dimension") { 100.0 }
+    val tA = Column("Calculation Time per Cell") { 10.0 }
+    val numberOfOperations = Column("Number of Operations") { 1.0 }
+    val tK = Column("Communication Time per Cell") { 200.0 }
+    fun timeParallel(x: Int): Double = (nX.eval(x) / numberOfCpus.eval(x)) * // Slice for each CPU
+            nY.eval(x) * // Whole Y-Dimension of the problem
+            tA.eval(x) * numberOfOperations.eval(x) + // Time to calculate each cell
+            tK.eval(x) * numberOfCpus.eval(x) // Communication increases with number of CPUs
 
-    spreadsheet {
-        column("Expenses", expenses)
-        column("Share of Expense", ::shareOfExpense)
-        column("Cost with (fictional) tax", ::expenseWithTaxes)
-    }.print(expenseList.size)
+    val tP = Column("Parallel Time", ::timeParallel)
+    fun timeSequential(x: Int): Double = nX.eval(x) * nY.eval(x) * tA.eval(x) * numberOfOperations.eval(x)
+    val tS = Column("Sequential Time", ::timeSequential)
+    fun calculateSpeedup(x: Int): Double = timeSequential(x) / timeParallel(x)
+    val speedup = Column("Speedup", ::calculateSpeedup)
+    fun calculateEfficiency(x: Int): Double = calculateSpeedup(x) / numberOfCpus.eval(x)
+    val efficiency = Column("Efficiency", ::calculateEfficiency)
+
+    val functions = listOf(numberOfCpus, nX, nY, tA, numberOfOperations, tK, tP, tS, speedup, efficiency)
+
+    document.body!!.append.div {
+        table {
+            tr {
+                repeat(10) { row ->
+                    th {
+                        +functions[row].title
+                    }
+                }
+            }
+            repeat(10) {row ->
+                tr {
+                    functions.forEach {
+                        td {
+                            +(it.eval(row)).toString()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
