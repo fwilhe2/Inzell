1d23b0e0b740f1d48ca3089d14282c069a3272bf
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-08-18 16:35:33 +0200

    no matrix build, add another test

--- src/commonTest/kotlin/SheetsTest.kt
+++ src/commonTest/kotlin/SheetsTest.kt
@@ -10,4 +10,18 @@ class SheetsTest {
         val sheet = spreadsheet { column("constant value") { 1.11 } }.row(0)
         assertEquals(expected, sheet)
     }
+
+    @Test
+    fun expenses() {
+        val expenseList = listOf(55.3, 82.1, 2.8, 9.0)
+        val expenses = buildFunctionOf(expenseList)
+        fun shareOfExpense(x: Int): Double = expenses(x) / expenseList.sum() * 100
+        fun expenseWithTaxes(x: Int): Double = expenses(x) * 1.15
+
+        spreadsheet {
+            column("Expenses", expenses)
+            column("Share of Expense", ::shareOfExpense)
+            column("Cost with (fictional) tax", ::expenseWithTaxes)
+        }.print(expenseList.size)
+    }
 }
